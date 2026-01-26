86501faeb8eab8e300aa0d76dbfee691263fb7e0
Giovanni Ruiz <termigiova@me.com>
2026-01-26 11:25:52 -0600

    feat: config dadbod

---
+++ nvim/lua/configs/dadbod.lua
@@ -0,0 +1,4 @@
+local data_dir = vim.fn.getcwd() .. "/.nvim"
+
+vim.g.db_ui_save_location = data_dir .. "/db_ui"
+vim.g.db_ui_use_nerd_fonts = 1
--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -181,9 +181,8 @@ return {
     lazy = true,
     dependencies = { "tpope/vim-dadbod" },
     cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
-    init = function()
-      vim.g.db_ui_use_nerd_fonts = 1
-      vim.g.db_ui_save_location = vim.fn.stdpath "data" .. "/db_ui"
+    config = function()
+      require "configs.dadbod"
     end,
   },
 
